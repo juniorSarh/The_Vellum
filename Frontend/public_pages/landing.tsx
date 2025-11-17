b6e2d2ce05fcab252bf9cc69cd0caabf1bf3773d
JuniorSarh <sabelohgumede@gmail.com>
2025-11-17 15:06:53 +0200

    feat: added logo under images folder

--- Frontend/public_pages/landing.tsx
+++ Frontend/public_pages/landing.tsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import Input from '../src/components/input'
 
 export default function Landing() {
   return (
     <div>
-      
+      <Input
+        label="Email"
+        type="email"
+        placeholder="Enter your email"
+        value=""
+        onChange={() => {}}
+      />
     </div>
-  )
+  );
 }
