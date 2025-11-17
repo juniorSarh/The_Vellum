6c696d7f6bcdfc7f344ddd961d9b8e8f7c1706b7
ntorh <ngcobontombenhle434@gmail.com>
2025-11-17 16:55:20 +0200

    feat : added form component for adding a hotel and its css

--- Frontend/public_pages/landing.tsx
+++ Frontend/public_pages/landing.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import HotelForm from '../src/components/hotelModal'
+
 
 export default function landing() {
   return (
     <div>
-      
+      <HotelForm/>
     </div>
   )
 }
---
+++ Frontend/src/components/hotelModal.tsx
@@ -0,0 +1,26 @@
+import "../HotelForm.css"; 
+
+function HotelForm() {
+  return (
+    <form className="hotel-form">
+      <div className="form-group">
+        <label>Hotel Name</label>
+        <input type="text" />
+      </div>
+
+      <div className="form-group">
+        <label>Location</label>
+        <input type="text" />
+      </div>
+
+      <div className="form-group">
+        <label>Amenities (Comma separated)</label>
+        <input type="text" />
+      </div>
+
+     
+    </form>
+  );
+}
+
+export default HotelForm;
