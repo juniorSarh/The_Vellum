80f24273a8fa75dce933b39195b2a83da17c9b38
Bongeka-Bhungane <angelabhungane@gmsil.com>
2025-11-17 16:48:17 +0200

    feat: added style to the footer component, and made responsive

--- Frontend/src/components/Footer.tsx
+++ Frontend/src/components/Footer.tsx
@@ -6,16 +6,17 @@ import Button from "./Button";
 export default function Footer() {
   return (
     <div className="footer-container">
-        <div className="footer-heading">
-          <h1>The Vellum</h1>
-        </div>
+      <div className="footer-text">
+        <h1>The Vellum</h1>
+        <p>@Copyright</p>
+      </div>
 
-        <div className="footer-icons">
-          <Button icon={<FaFacebook />} />
-          <Button icon={<FaInstagram />} />
-          <Button icon={<FaTwitter />} />
-        </div>
-        
+      <div className="footer-icons">
+        <span>Contact Us</span>
+        <Button icon={<FaFacebook />} className="footer-buttons" />
+        <Button icon={<FaInstagram />} className="footer-buttons" />
+        <Button icon={<FaTwitter />} className="footer-buttons" />
+      </div>
     </div>
   );
 }
