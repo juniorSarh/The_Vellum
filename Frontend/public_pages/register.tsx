c3eb4a3860cacdcfb7d27e29b3f9b8ffc8ce3c35
Melokuhle <melokuhlemaphisa99@gmail.com>
2025-11-18 09:28:01 +0200

    feat:Added a style for modal

--- Frontend/public_pages/register.tsx
+++ Frontend/public_pages/register.tsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, { useState } from "react";
+import ProfileModal from "../src/components/profileModal";
+import Button from "../src/components/Button";
 
 export default function Register() {
+  const [open, setOpen] = useState(false);
+
   return (
-    <div>
-      
-    </div>
-  )
+    <>
+      <Button
+        name="Edit Profile"
+        backgroundColor="black"
+        color="white"
+        onClick={() => setOpen(true)}
+      />
+
+      <ProfileModal isOpen={open} onClose={() => setOpen(false)} />
+    </>
+  );
 }
--- Frontend/src/components/profileModal.tsx
+++ Frontend/src/components/profileModal.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Button from "../components/Button";
-import "../EditModal.css";
+import "../ProfileModal.css";
 
 interface UpdateModalProps {
   isOpen: boolean;
