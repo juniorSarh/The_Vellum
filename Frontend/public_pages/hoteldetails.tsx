724a28ad7b9f1923b25b8ef4807dd1937b3b7b50
ntorh <ngcobontombenhle434@gmail.com>
2025-11-19 15:43:11 +0200

    feat: added add admin page

--- Frontend/public_pages/hoteldetails.tsx
+++ Frontend/public_pages/hoteldetails.tsx
@@ -1,4 +1,9 @@
-export default function hoteldetails() {
-  return <div></div>;
-}
+import React from 'react'
 
+export default function hoteldetails() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
