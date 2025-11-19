08cca36357e3fa8de0c7aa4bbb89a8ec673e40b7
JuniorSarh <sabelohgumede@gmail.com>
2025-11-19 15:22:48 +0200

    Updated the customer files after being removed

---
+++ Backend/src/routes/customer.routes.ts
@@ -0,0 +1,17 @@
+import { Router } from "express";
+import * as customerController from "../controllers/customer.controller";
+
+export const customerRouter = Router();
+
+// Authentication endpoints
+customerRouter.post("/register", customerController.registerCustomer);
+customerRouter.post("/login", customerController.loginCustomer);
+
+// Profile management endpoints
+customerRouter.get("/", customerController.getCustomers);
+customerRouter.get("/:id", customerController.getProfile);
+customerRouter.patch("/:id", customerController.updateProfile);
+customerRouter.delete("/:id/deactivate", customerController.deactivateAccount);
+customerRouter.patch("/:id/deactivate", customerController.deactivateAccount);
+
+export default customerRouter;
