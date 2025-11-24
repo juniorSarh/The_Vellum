f9095f3fd65f300caedb60f2c635168b0735fb9a
Bongeka-Bhungane <angelabhungane@gmsil.com>
2025-11-24 10:47:34 +0200

    feat:fixed the styling on the checkout page and navigation on the pay and cancel buttons

--- Frontend/Customer-app/pages/checkout.tsx
+++ Frontend/Customer-app/pages/checkout.tsx
@@ -1,16 +1,14 @@
-import PrivateNavBar from "../../src/components/PrivateNaveBar";
 import Footer from "../../src/components/Footer";
 import Button from "../../src/components/Button";
 import Input from "../../src/components/input";
 import "../../src/assets/css/checkout.css";
+import PrivatNav from "../../src/components/PrivatNav";
+import { Link } from "react-router-dom";
 
 export default function Checkout() {
   return (
     <div className="checkout-container">
-      {/* NAVBAR */}
-      <div className="nav">
-        <PrivateNavBar />
-      </div>
+        <PrivatNav />
 
       <div className="checkout-content">
         {/* TOP SECTION: IMAGE + HOTEL DETAILS */}
@@ -36,54 +34,64 @@ export default function Checkout() {
 
         {/* FORM SECTION */}
         <div className="checkout-form">
-          <div className="form">
+          <form className="form">
+            {/* COLUMN 1 */}
             <div className="form-column">
+              <Input type="name" placeholder="Enter your Name" label="Name" />
               <Input
-                placeholder="Enter Name & Surname"
-                label="Name & Surname"
+                type="surname"
+                placeholder="Enter your Surname"
+                label="Surname"
               />
               <Input
-                placeholder="Please Provide Email"
                 type="email"
+                placeholder="Please Provide Email"
                 label="Email"
               />
               <Input
                 label="Number of Rooms"
-                placeholder="Number of Rooms"
                 type="number"
+                placeholder="Number of Rooms"
               />
               <Input
                 label="Number of People"
-                placeholder="Number of People"
                 type="number"
+                placeholder="Number of People"
               />
             </div>
 
+            {/* ROW 1 */}
             <div className="form-row">
               <Input label="ID Number" placeholder="Please Enter ID Number" />
-              <Input placeholder="Check-in Time" type="time" />
+              <Input label="Check-in Time" type="time" placeholder="Time" />
             </div>
 
+            {/* ROW 2 */}
             <div className="form-row">
               <Input label="Check-in Date" type="date" />
               <Input label="Check-out Date" type="date" />
             </div>
-          </div>
+          </form>
 
           {/* BUTTONS */}
           <div className="checkout-buttons">
-            <Button
-              name="Pay"
-              color="white"
-              backgroundColor="#846d29"
-              className="btn"
-            />
-            <Button
-              name="Cancel"
-              color="white"
-              backgroundColor="Red"
-              className="btn"
-            />
+            <Link to="/payment">
+              <Button
+                name="Pay"
+                color="white"
+                backgroundColor="#846d29"
+                className="btn"
+              />
+            </Link>
+
+            <Link to="/hotel">
+              <Button
+                name="Cancel"
+                color="white"
+                backgroundColor="red"
+                className="btn"
+              />
+            </Link>
           </div>
         </div>
       </div>
